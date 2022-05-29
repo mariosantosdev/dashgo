f1c436a1271769ab2e8a171157e12c1a6370e3ff
Mário Santos <mariodev7@gmail.com>
2022-05-29 17:35:40 -0300

    create sign in page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,35 @@
+import { Button, Flex, Stack } from "@chakra-ui/react";
+
 import Head from "next/head";
+import { Input } from "~components/Form/Input";
 
-export default function Home() {
+export default function SignIn() {
   return (
-    <div>
+    <>
       <Head>
-        <title>DashGO</title>
+        <title>DashGO - Login</title>
       </Head>
+      <Flex w="100vw" h="100vh" align="center" justify="center">
+        <Flex
+          as="form"
+          flexDir="column"
+          width="100%"
+          maxWidth={360}
+          bg="gray.800"
+          p={8}
+          borderRadius={8}
+        >
+          <Stack spacing={4} mb={6}>
+            <Input name="email" type="email" label="Email" />
 
-      <h1>DashGO</h1>
-    </div>
+            <Input name="password" type="password" label="Senha" />
+          </Stack>
+
+          <Button type="submit" size="lg" colorScheme="pink">
+            Entrar
+          </Button>
+        </Flex>
+      </Flex>
+    </>
   );
 }
