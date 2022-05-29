df16aa76e9a53c51bac180c532c00bc27c9ed571
Mário Santos <mariodev7@gmail.com>
2022-05-29 17:05:07 -0300

    create base project

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>DashGO</title>
+      </Head>
+
+      <h1>DashGO</h1>
+    </div>
+  );
+}
