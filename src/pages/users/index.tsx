29b8bfd565d44d5d92314b39e589144c44e7f4c4
Mário Santos <mariodev7@gmail.com>
2022-05-29 18:23:54 -0300

    create table users

---
+++ src/pages/users/index.tsx
@@ -0,0 +1,93 @@
+import {
+  Flex,
+  Box,
+  Heading,
+  Button,
+  Icon,
+  Table,
+  Thead,
+  Tr,
+  Th,
+  Checkbox,
+  Tbody,
+  Td,
+  Text,
+} from "@chakra-ui/react";
+import Head from "next/head";
+import { RiAddLine, RiPencilLine } from "react-icons/ri";
+import { Header } from "~components/Header";
+import { Sidebar } from "~components/Sidebar";
+
+export default function UserList() {
+  return (
+    <Box>
+      <Head>
+        <title>DashGO</title>
+      </Head>
+      <Header />
+
+      <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
+        <Sidebar />
+
+        <Box flex="1" borderRadius={8} bg="gray.800" p="8">
+          <Flex mb="8" justify="space-between" align="center">
+            <Heading size="lg" fontWeight="normal">
+              Usuários
+            </Heading>
+            <Button
+              as="a"
+              size="sm"
+              fontSize="sm"
+              colorScheme="pink"
+              leftIcon={<Icon as={RiAddLine} fontSize="20" />}
+            >
+              Criar Novo
+            </Button>
+          </Flex>
+
+          <Table colorScheme="whiteAlpha">
+            <Thead>
+              <Tr>
+                <Th px="6" color="gray.300" width="8">
+                  <Checkbox colorScheme="pink" />
+                </Th>
+                <Th>Usuário</Th>
+                <Th>Data de cadastro</Th>
+                <Th w="8"></Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              <Tr>
+                <Td px="6">
+                  <Checkbox colorScheme="pink" />
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Mário Santos</Text>
+                    <Text fontSize="sm" color="gray.300">
+                      mariodev7@gmail.com
+                    </Text>
+                  </Box>
+                </Td>
+                <Td>
+                  <Text>16/03/2022</Text>
+                </Td>
+                <Td>
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="sm"
+                    colorScheme="blackAlpha"
+                    leftIcon={<Icon as={RiPencilLine} />}
+                  >
+                    Editar
+                  </Button>
+                </Td>
+              </Tr>
+            </Tbody>
+          </Table>
+        </Box>
+      </Flex>
+    </Box>
+  );
+}
