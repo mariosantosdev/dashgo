eeb6189228a895a92de32f366aa951e05ad4a628
Mário Santos <mariodev7@gmail.com>
2022-05-29 18:28:46 -0300

    pagination component

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -16,6 +16,7 @@ import {
 import Head from "next/head";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { Header } from "~components/Header";
+import { Pagination } from "~components/Pagination";
 import { Sidebar } from "~components/Sidebar";
 
 export default function UserList() {
@@ -86,6 +87,8 @@ export default function UserList() {
               </Tr>
             </Tbody>
           </Table>
+
+          <Pagination />
         </Box>
       </Flex>
     </Box>
