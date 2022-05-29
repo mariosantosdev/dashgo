8bed89bbc907c3892a5358d57cdbd9d19143bb19
Mário Santos <mariodev7@gmail.com>
2022-05-29 17:59:10 -0300

    create sidebar component

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,13 +1,20 @@
+import { Flex } from "@chakra-ui/react";
 import Head from "next/head";
+
 import { Header } from "~components/Header";
+import { Sidebar } from "~components/Sidebar";
 
 export default function Dashboard() {
   return (
-    <>
+    <Flex direction="column" h="100vh">
       <Head>
         <title>DashGO</title>
       </Head>
       <Header />
-    </>
+
+      <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
+        <Sidebar />
+      </Flex>
+    </Flex>
   );
 }
