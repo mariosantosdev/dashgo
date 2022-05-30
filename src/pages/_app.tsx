327d7fe0f6d6b1fe233a38d481eca9888bf5af41
Mário Santos <mariodev7@gmail.com>
2022-05-30 19:26:37 -0300

    setup mirage server

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,13 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import { AppProps } from "next/app";
 import { SidebarDrawerProvider } from "src/contexts/SidebarDrawerContext";
+
+import { makeServer } from "~services/mirage";
 import { theme } from "~styles/theme";
+
+if (process.env.NODE_ENV === "development") {
+  makeServer();
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
