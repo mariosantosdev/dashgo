919ea04cb39e090901be2c45f2701a3cc023b5d5
Mário Santos <mariodev7@gmail.com>
2022-05-30 00:04:57 -0300

    add navigation to navlink component

--- src/components/Sidebar/SidebarNav.tsx
+++ src/components/Sidebar/SidebarNav.tsx
@@ -14,14 +14,22 @@ export function SidebarNav() {
     <Stack spacing="12" align="flex-start">
       {/* Geral */}
       <NavSection title="Geral">
-        <NavLink icon={RiDashboardLine}>Dashboard</NavLink>
-        <NavLink icon={RiContactsLine}>Usuários</NavLink>
+        <NavLink icon={RiDashboardLine} href="/dashboard">
+          Dashboard
+        </NavLink>
+        <NavLink icon={RiContactsLine} href="/users">
+          Usuários
+        </NavLink>
       </NavSection>
 
       {/* Automation */}
       <NavSection title="Automação">
-        <NavLink icon={RiInputMethodLine}>Formulários</NavLink>
-        <NavLink icon={RiGitMergeLine}>Automação</NavLink>
+        <NavLink icon={RiInputMethodLine} href="/forms">
+          Formulários
+        </NavLink>
+        <NavLink icon={RiGitMergeLine} href="/automation">
+          Automação
+        </NavLink>
       </NavSection>
     </Stack>
   );
