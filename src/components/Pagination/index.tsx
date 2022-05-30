72c21f43455bfec2f9cdccee24807140624d3474
Mário Santos <mariodev7@gmail.com>
2022-05-29 23:55:41 -0300

    responsive pagination component

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -1,10 +1,16 @@
-import { Box, HStack } from "@chakra-ui/react";
+import { Box, Stack, HStack } from "@chakra-ui/react";
 
 import { PaginationItem } from "./PaginationItem";
 
 export function Pagination() {
   return (
-    <HStack mt="8" justify="space-between" align="center" spacing="6">
+    <Stack
+      direction={["column", "row"]}
+      mt="8"
+      justify="space-between"
+      align="center"
+      spacing="6"
+    >
       <Box>
         <strong>0</strong> - <strong>10</strong> de <strong>100</strong>
       </Box>
@@ -14,6 +20,6 @@ export function Pagination() {
         <PaginationItem pageNumber="3" />
         <PaginationItem pageNumber="4" />
       </HStack>
-    </HStack>
+    </Stack>
   );
 }
